980bd33509c7744bc035805301c2a51ed085eb8b
Pere Ripoll <pereriluq@gmail.com>
2024-01-27 18:18:25 +0100

    res

--- agenda_ArturShakirov-PereRipoll/agenda/obj/Debug/net8.0/agenda.AssemblyInfo.cs
+++ agenda_ArturShakirov-PereRipoll/agenda/obj/Debug/net8.0/agenda.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("agenda")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+defa0fb81f5c7b2c7c70bd5900924002b8044571")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6965c70efe7ab826eb134b5a08621511c9453e2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("agenda")]
 [assembly: System.Reflection.AssemblyTitleAttribute("agenda")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
