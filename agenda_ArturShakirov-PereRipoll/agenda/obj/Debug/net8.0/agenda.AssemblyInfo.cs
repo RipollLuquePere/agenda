8bb77129eac105f0fa7ea8dafdce2780484190e7
Pere Ripoll <pereriluq@gmail.com>
2024-02-03 14:49:49 +0100

    Igualar canvis amb arturShakirov

--- agenda_ArturShakirov-PereRipoll/agenda/obj/Debug/net8.0/agenda.AssemblyInfo.cs
+++ agenda_ArturShakirov-PereRipoll/agenda/obj/Debug/net8.0/agenda.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("agenda")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f3d6956d04f8b4c67b0908be9f007404026768e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8953d3d25f99b111f56b2c2a5c5304d54751fe1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("agenda")]
 [assembly: System.Reflection.AssemblyTitleAttribute("agenda")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
